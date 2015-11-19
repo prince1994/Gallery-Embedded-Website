6484cbb003f25391ecec341c7bcaf989b8dc7407
prince1994 <ranjansingh119@gmail.com>
2015-11-19 21:52:23 +0530

    Responsive-Website
    A site created for the beginner by a beginner!!

---
+++ webgallery/image_upload.php
@@ -0,0 +1,7 @@
+<form action="image_upload.php" method="POST" enctype="multipart/form-data">
+Choose your file<input type="file" name="uploaded_file"/><br/><br/>
+<input type="submit" value="Upload"/><br/>
+</form>
+
+
+Please browse a file before cicking the browse button
